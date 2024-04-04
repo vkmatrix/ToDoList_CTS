67255c0dd90d4d9eb15ed2663d31e5acf799bc50
Balajiraman2303 <107234628+Balajiraman2303@users.noreply.github.com>
2024-04-04 16:06:19 +0530

    Update ToDoList.py

--- ToDoList.py
+++ ToDoList.py
@@ -1,1 +1,9 @@
 # To Do List
+class TodoList:
+    def __init__(self):
+        self.tasks = []
+
+    def add_task(self, task):
+        self.tasks.append({'task': task, 'completed': False})
+        print("Task added successfully.")
+      
